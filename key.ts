1534fa389608a24c040a59fc69ba9b7a659003e1
nqhuy19 <112528789+nqhuy19@users.noreply.github.com>
2023-01-09 21:18:01 +0700

    test upload to firebase

---
+++ key.ts
@@ -0,0 +1,14 @@
+const key = {
+  type: "service_account",
+  project_id: "fragments-6246",
+  private_key_id: "050ff103f7739e65309393fce69f4749929c8165",
+  private_key: "-----BEGIN PRIVATE KEY-----\nMIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQC0ejMQRPD9dfcj\nf6pMXS/GITAAN9DxwLKmOSvTx1LWrrdXYv4RJF4s89vt33Wmfq37soDtFPdNxtj7\nazuI+wJzv0rDjGDJu7/tNE8QJcTSFoTH8ISzwlX55W6iQlOhNKgGxNJ0ki96H836\nTGxZM+pEYvPAxZX9Tkkxb2i+05hwhqXsyfSXor1eZlS1K2dS54YxroPT4AbNrixH\niznJwrphCHTPpea8xLBNXw1T8XL87sMPHuusGcV3MxWyl5uFHeWkhP+KZqk20oBk\nMhdqhWRUYE404sETsMYg5AB1MzxgOAAAOyaRfRG5CM1D1GZmxVg4ZEAvxb3l7WP8\nmOCmE0jfAgMBAAECggEAARK07loqlfyMkqEgscGKwNfTrDyouQyXQ1ZrBKBWcBZW\nhWYo8pB6dYlMUSFsq/0W1bydovXmnodvJNeMtrzJ//GEOp4WuTxXi3aDKH/A/YAg\nG/w5yG6AX7MAhxBz5NfQ+Uappa3KKyS4mc4N2t3sfP7wShYiqoCQ/i1BEdlk5PkK\nwbC98iZyVPGGT8VgAKBazyzYBhzPqeMD5jI7I7PHO8fbFMmtbAVyUiBs+b6iGIQY\n8x4pfoEvbdZWgG1Mr63zTjt67SdSBYBYnVS5pO/gV7+OWaO7fq6+pVr1beJ8K0k/\nhHp6v8FnsWIzCKKwz0c5NFaZpOHcS1AXB6uXz9a/aQKBgQDbZwAymS0xN1ExwiR0\n7mBn5RrM0bkZvCphU+/gPQGR9uhK+B6AApbwubfc55gJcbIWt+o+P5lWC/e6vwn4\nZ0MOw/eCtqYX63uz3akQqrru0bOpVUgvKL3sJX1Aj7FHSDDCmkNfSKgXmS48Oc/2\n03V0hwMAyDhtNViDS0Ofmj2+2wKBgQDSlQJG/ft42B8K4pb49iQRP+JrtEy9TtL9\nLjX0o5Vm5cvxW4US1TNvtOMgGyRNakLv3gfR/sduhzJffFWAT5Xd+9kncUNordy+\nuOYdFtN5CrvooctRCvudQYTkkiDowwE8s7YmnCqG2vFJUZbd95aGeMon3+Ji+JiY\nJTUvBjXzTQKBgQCiehueXOsnuFRrqd8/rMw+BF8dcO5HzZ4A550LjYIdKyEJa8eC\neuh2qsIrWGKyE72Tr21L3eNa4o/5fsIOb8IsLBmedk54Bk5ce/lYu2altyv6aYqE\nSgKj33trFT5ukFpzsrPT20WH/EtOFG4a1qvzuIwTxzJqRYgf6tvQVPQG4wKBgQCP\ns/YI4jugvMN6VyS1oqr8Rn9KBpEzwr3C3q0dVCCZRooFG/JU5r8iJOu1ti/w0Gn7\nbKYjjN0ltQcQGDX0yNq50Krg+q8ImVUBpgYKpdAMUuHmn7qyhG8uYk7A+GOS07hd\nZzoPYRS4UGLGKPJuaQ4EmBtEWMkRxxnDAekoPPHGJQKBgQCoGsQCDKREBTFgCdro\nB9DCqaC+4ehPFg9AkVk1yvwSSnqw1U42+LYLrKt3VfWedKB/8dEbHZELuDyzzBLt\nSA8utxI/7jTkDc4v1nJYqm6oYfZL3ysv+iMd0fg1TGGn1p0xzG9R/8nqmlS3BRx/\njQdKb36Dp1MUf2M+N9FL/l+Dtw==\n-----END PRIVATE KEY-----\n",
+  client_email: "firebase-adminsdk-muitu@fragments-6246.iam.gserviceaccount.com",
+  client_id: "117542878383056817537",
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-muitu%40fragments-6246.iam.gserviceaccount.com"
+}
+
+export default key;
--- src/routers/admin/song.router.ts
+++ src/routers/admin/song.router.ts
@@ -1,8 +1,9 @@
 import express from "express";
 import multer from "multer";
 import SongController from "../../controllers/admin/song.controller";
+import firebase from '../../middlewares/firebase';
 
-let upload = multer();
+let upload = multer({storage: multer.memoryStorage()});
 let router = express.Router();
 
 
@@ -11,7 +12,7 @@ router.get('/ShowAddSong', SongController.showAddSong);
 router.post('/', SongController.addSong);
 router.get('/:id', SongController.getSong);
 router.put('/:id', upload.none(), SongController.updateSong);
-
+router.post('/upload', upload.single('file'), firebase);
 
 
 export default router;
