3e0319c1722dfa10f47266d277ab9373030d86c7
nguyentheanhtlu <theanh28022003@gmail.com>
2023-01-17 15:58:17 +0700

    latesong

--- FrontEnd/src/components/Main/LatestSong/ListSong.jsx
+++ FrontEnd/src/components/Main/LatestSong/ListSong.jsx
@@ -4,8 +4,10 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
-import getSong from '../../service/ListSong';
+import {getSong} from '../../../services/ListSong';
 import {useEffect, useState} from "react";
+import Backdrop from '@mui/material/Backdrop';
+import CircularProgress from '@mui/material/CircularProgress';
 
 
 import * as React from 'react';
@@ -13,29 +15,7 @@ import { experimentalStyled as styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
-
-const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-    ...theme.typography.body2,
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-}));
-
-export function ResponsiveGrid() {
-    return (
-        <Box sx={{ flexGrow: 1 }}>
-            <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-                {Array.from(Array(6)).map((_, index) => (
-                    <Grid item xs={2} sm={4} md={4} key={index}>
-                        <Item>xs=2</Item>
-                    </Grid>
-                ))}
-            </Grid>
-        </Box>
-    );
-}
-
+import Link from "@mui/material/Link";
 
 export default function ActionAreaCard() {
     const [song,setSongs] = useState([]);
@@ -51,11 +31,6 @@ export default function ActionAreaCard() {
             <>
                 <Box sx={{ flexGrow: 1 }}>
                     <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-                        {/*{Array.from(Array(6)).map((_, index) => (*/}
-                        {/*    <Grid item xs={2} sm={4} md={4} key={index}>*/}
-                        {/*        <Item>xs=2</Item>*/}
-                        {/*    </Grid>*/}
-                        {/*))}*/}
                         {song.data.map((item, index) =>{
                             return (<Grid item xs={2} sm={4} md={4} key={index}>
                                 <Card sx={{ marginLeft :'200px', marginBottom: '10px'} }>
@@ -68,10 +43,13 @@ export default function ActionAreaCard() {
                                     />
                                     <CardContent>
                                         <Typography gutterBottom variant="h5" component="div">
-
+                                            {item.title}
                                         </Typography>
                                         <Typography variant="body2" color="text.secondary">
-                                            {item.title}
+                                            {item.artists.map(item =>(
+                                                <span>{item.name}  </span>
+                                            ))
+                                            }
                                         </Typography>
                                     </CardContent>
                                 </CardActionArea>
@@ -84,6 +62,13 @@ export default function ActionAreaCard() {
             </>
         );
     } else {
-        return <p>Loading..</p>
+        return(
+        <Backdrop
+            sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+            open={song.length === 0}
+        >
+            <CircularProgress color="inherit" />
+        </Backdrop>
+        )
     }
 }
