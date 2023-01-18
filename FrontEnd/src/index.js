df924c827ae176e445c77aded0e8a3a8cc6a495b
nqhuy19 <112528789+nqhuy19@users.noreply.github.com>
2023-01-18 08:51:25 +0700

    apply template

--- FrontEnd/src/index.js
+++ FrontEnd/src/index.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
-import { store } from './store';
-import { Provider } from 'react-redux';
+import { Provider } from 'react-redux'
+import store  from './store';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+
+ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <App />
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
