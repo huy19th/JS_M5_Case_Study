29f6a638a5726efc7e85ae56bf9bb65052ec8d81
nguyentheanhtlu <theanh28022003@gmail.com>
2023-01-30 11:38:40 +0700

    seeall

--- FrontEnd/src/services/ListSong.js
+++ FrontEnd/src/services/ListSong.js
@@ -5,9 +5,16 @@ axios.defaults.baseURL = "http://localhost:8080/api/song"
 export const getSong = async () => {
     return await axios.get("/");
 }
+export const getSongR = async () => {
+    return await axios.get("/all");
+}
 export const getSongVietNam = async () => {
     return await axios.get("/country/Vietnam");
 }
+export const getSongVietNamR = async () => {
+    return await axios.get("/country/Vietnam/R");
+}
+
 export const getSongNotCountry = async () => {
     return await axios.get("/country/not/Vietnam")
 }
@@ -15,3 +22,9 @@ export const getSongNotCountry = async () => {
 export const getTrendingSong = async () => {
     return await axios.get("/trendingSongs");
 }
+export const getTrendingSongR = async () => {
+    return await axios.get("/trendingSongsR");
+}
+
+
+
