eeede67c192c75b86cda5e3ac84b785133251812
nqhuy19 <112528789+nqhuy19@users.noreply.github.com>
2023-01-31 14:49:26 +0700

    add vertical gap between cards

--- FrontEnd/src/pages/SeeAll.js
+++ FrontEnd/src/pages/SeeAll.js
@@ -31,7 +31,7 @@ const SeeAll = () => {
             <section className='mb-4 min-w-full'>
                 <h3 className={`text-2xl font-text-white tracking-tight font-sans hover`}>{title}</h3>
 
-                <div className='grid grid-cols-5 gap-x-6'>
+                <div className='grid grid-cols-5 gap-x-6 gap-y-6'>
                     {songs.map(item => <CardSection item={item} key={item.id}/>)}
                 </div>
 
